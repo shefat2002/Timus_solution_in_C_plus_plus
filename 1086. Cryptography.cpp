8505512248bc7ed2f700af3d3e140e2d61579ec8
Shefat Al Mahmud <shefat2002@gmail.com>
2022-05-23 11:40:19 +0600

    Create 1086. Cryptography.cpp

---
+++ 1086. Cryptography.cpp
@@ -0,0 +1,39 @@
+//1086. Cryptography
+//Please try yourself before watch this code
+
+#include <bits/stdc++.h>
+using namespace std;
+#define mx 1000004
+
+void sieve(vector<int>&p)
+{
+    bool isprime[mx];
+    memset(isprime , true , sizeof(isprime));
+
+    for(int i = 2; i*i < mx ; i++){
+        if(isprime[i] == true){
+            for(int j = i*i ; j < mx; j+=i){
+                isprime[j] = false;
+            }
+        }
+    }
+    for(int i = 2 ; i < mx ; i++){
+        if(isprime[i]== true) p.push_back(i);
+    }
+}
+
+int main()
+{
+    int n ;
+    vector<int>p;
+    sieve(p);
+    cin >> n;
+    while(n--){
+        
+        int k;
+        cin >> k;
+        cout << p[k-1] << endl;
+    }
+
+    return 0;
+}
